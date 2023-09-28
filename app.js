c497e1378f0188b7f1490a954003d544d13e58aa
Rajengrad <rajen.grad26@gmail.com>
2023-09-28 11:35:08 +0530

    added changes to JS file added procfile for heroku

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.post("/", (req,res)=>{
         res.redirect("/")
     })
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Server is listening at port 3000")
 });
 
